b519e90500c3a96ac12a0482d7afc8c249be7780
Chandan <n.ranganath.chandan@gmail.com>
2026-02-28 05:10:14 +0530

    Shifted from Single inserts to Bulk Loads

--- src/db.py
+++ src/db.py
@@ -1,78 +1,53 @@
 import psycopg2
+from psycopg2.extras import execute_values
+
 from config import DB_URL
 
 def get_connection():
     return psycopg2.connect(DB_URL)
-    
-def insert_repo(repo_data: dict):
-    conn = get_connection()
-    cursor = conn.cursor()
-    
-    query = """
-            INSERT INTO repos (id, company_id, name, full_name, language, created_at, updated_at)
-            VALUES (%s,%s,%s,%s,%s,%s,%s) 
-            ON CONFLICT (id) DO NOTHING;        
-        """
-    
-    values = (
-        repo_data["id"],
-        repo_data["company_id"],
-        repo_data["name"],
-        repo_data["full_name"],
-        repo_data["language"],
-        repo_data["created_at"],
-        repo_data["updated_at"]
-    )
-    
-    cursor.execute(query,values)
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print("Inserted repos")
-    
-def insert_company(company_data: dict):
-    conn = get_connection()
-    cursor = conn.cursor()
 
+def bulk_insert_companies(conn, companies:dict):
+    if not companies: return
+    
+    cursor = conn.cursor()
+    
     query = """
-            INSERT INTO companies (id, name)
-            VALUES (%s, %s)
+            INSERT INTO companies(id, name)
+            VALUES %s
             ON CONFLICT (id) DO NOTHING;
         """
-
-    values = (
-        company_data["id"],
-        company_data["name"],
-    )
-
-    cursor.execute(query, values)
-    conn.commit()
+    
+    values = [(cid, name) for cid, name in companies.items()]
+    
+    execute_values(cursor, query, values)
     cursor.close()
-    conn.close()
-    print("Inserted company")
-
-def insert_repo_snapshot(snapshot_data: dict):
-    conn = get_connection()
+    
+def bulk_insert_repos(conn, repos:dict):
+    if not repos: return
+    cursor = conn.cursor()
+    
+    query = """
+            INSERT INTO repos(id, company_id, name, full_name, language, created_at, updated_at)
+            VALUES %s
+            ON CONFLICT (id) DO NOTHING;
+        """
+    
+    values = list(repos.values())
+    
+    execute_values(cursor, query, values)
+    cursor.close()
+    
+def bulk_insert_snapshots(conn, snapshots: list):
+    if not snapshots: return
     cursor = conn.cursor()
     
     query = """
             INSERT INTO repo_snapshots(
                 repo_id, snapshot_date, stars, forks, open_issues
             )
-            VALUES (%s, %s, %s, %s, %s)
+            VALUES %s
             ON CONFLICT (repo_id, snapshot_date) DO NOTHING;
         """
   
-    values = (
-        snapshot_data["repo_id"],
-        snapshot_data["snapshot_date"],
-        snapshot_data["stars"],
-        snapshot_data["forks"],
-        snapshot_data["open_issues"],
-    )
-    
-    cursor.execute(query,values)
-    conn.commit()
+    execute_values(cursor, query, snapshots)
     cursor.close()
-    conn.close()
-    print("Inserted snapshot")
--- src/ingest.py
+++ src/ingest.py
@@ -1,41 +1,55 @@
 import sys
+from datetime import date
+
 from github_client import GitHubClient
-from db import insert_repo, insert_company, insert_repo_snapshot
+from db import get_connection, bulk_insert_repos, bulk_insert_companies, bulk_insert_snapshots
 
 def run_ingestion(username:str):
+    conn = get_connection()
     
-    client = GitHubClient()
-
-    repos = client.get_user_repos(username)
+    try:
+        client = GitHubClient()
+        repos = client.get_user_repos(username)
+        companies = {}
+        repos_to_insert = {}
+        snapshots = []
     
-    for repo in repos :
+        for repo in repos :
+            
+            company_id = repo["owner"]["id"]
+            company_name = repo["owner"]["login"]
+            
+            companies[company_id] = company_name
+            
+            repos_to_insert[repo["id"]] = (
+                repo["id"],
+                company_id,
+                repo["name"],
+                repo["full_name"],
+                repo["language"],
+                repo["created_at"],
+                repo["updated_at"],
+            )
+            
+            snapshots.append((
+                repo["id"],
+                date.today(),
+                repo["stargazers_count"],
+                repo["forks_count"],
+                repo["open_issues_count"]
+            ))
         
-        company_data = {
-            "id": repo["owner"]["id"],
-            "name": repo["owner"]["login"],
-        }
+        bulk_insert_companies(conn, companies)
+        print("Inserted companies")
+        bulk_insert_repos(conn, repos_to_insert)
+        print("Inserted repos")
+        bulk_insert_snapshots(conn, snapshots)
+        print("Inserted snapshots")
         
-        repo_data = {
-            "id": repo["id"],
-            "company_id": repo["owner"]["id"],
-            "name": repo["name"],
-            "full_name": repo["full_name"],
-            "language": repo["language"],
-            "created_at": repo["created_at"],
-            "updated_at": repo["updated_at"],
-        }
-        
-        snapshot_data ={
-            "repo_id" : repo["id"],
-            "snapshot_date": repo["created_at"],
-            "stars": repo["stargazers_count"],
-            "forks": repo["forks_count"],
-            "open_issues": repo["open_issues_count"],
-        }
-    
-        insert_company(company_data)
-        insert_repo(repo_data)
-        insert_repo_snapshot(snapshot_data)
+        conn.commit()
+            
+    finally:
+        conn.close()
          
 if __name__ == "__main__":
     run_ingestion(sys.argv[1])
